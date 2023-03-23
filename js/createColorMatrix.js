92e729951e142c22bd18cb61861e115b954a3a47
Nikita-Tikhomirov <50404447+Nikita-Tikhomirov@users.noreply.github.com>
2023-03-23 13:16:59 +0300

    Fix errors

--- js/createColorMatrix.js
+++ js/createColorMatrix.js
@@ -3,42 +3,46 @@ const colorPickerToSvg = document.querySelector('#foo')
 colorPickerToSvg.addEventListener('input', () => {
     
     const activeImgWrap = document.querySelector('.generated-img-wrap.active')
-    const activeImgWrapId = activeImgWrap.id
-    let clearId = activeImgWrapId.replace(/\D/g, '');
+    const svgInside = activeImgWrap.querySelector('svg')
+    if(svgInside != undefined){
+        const activeImgWrapId = activeImgWrap.id
+        let clearId = activeImgWrapId.replace(/\D/g, '');
+        
     
-
-
-    const setPrimaryColor = function (_r, _g, _b) {
-
-        const rScaled = _r / 255.0;
-        const gScaled = _g / 255.0;
-        const bScaled = _b / 255.0;
     
-        const feColorMatrixElem = document.getElementById(`filter_count_${clearId}`).getElementsByTagName('feColorMatrix')[0];
-        feColorMatrixElem.setAttribute(
-            `values`,
-            `0 0 0 0 ${rScaled}
-                 0 0 0 0 ${gScaled}
-                 0 0 0 0 ${bScaled}
-                 0.2 0.2 0.2 0 0`
-        );
-    };
+        const setPrimaryColor = function (_r, _g, _b) {
     
-    function hexToRgb(hex) {
-        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
+            const rScaled = _r / 255.0;
+            const gScaled = _g / 255.0;
+            const bScaled = _b / 255.0;
+        
+            const feColorMatrixElem = document.getElementById(`filter_count_${clearId}`).getElementsByTagName('feColorMatrix')[0];
+            feColorMatrixElem.setAttribute(
+                `values`,
+                `0 0 0 0 ${rScaled}
+                     0 0 0 0 ${gScaled}
+                     0 0 0 0 ${bScaled}
+                     0.2 0.2 0.2 0 0`
+            );
+        };
+        
+        function hexToRgb(hex) {
+            const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        }
+        
+        
+    
+        console.log(colorPickerToSvg.value);
+        let hexNow = colorPickerToSvg.value
+        const color = hexToRgb(hexNow);
+        setPrimaryColor(color.r, color.g, color.b); 
     }
-    
-    
-
-    console.log(colorPickerToSvg.value);
-    let hexNow = colorPickerToSvg.value
-    const color = hexToRgb(hexNow);
-    setPrimaryColor(color.r, color.g, color.b); 
+  
 })
 
 
